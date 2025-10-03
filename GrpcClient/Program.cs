6344d0be31dbffd1df4ae298e770b2dbb2c894d9
Vladislav East <vladislav.sage@yandex.ru>
2025-10-03 20:09:39 +0300

    Start

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,37 @@
+﻿using Grpc.Net.Client;
+using GrpcServer;
+using System.Linq.Expressions;
+
+
+namespace GrpcClient
+{
+	internal class Program
+	{
+		static async Task Main(string[] args)
+		{
+			var channel = GrpcChannel.ForAddress("https://localhost:7236");
+
+			var client = new Greeter.GreeterClient(channel);
+
+			var reply = await client.SayHelloAsync(
+						new HelloRequest { Name = "gRPC Client" }
+					);
+
+
+			Console.WriteLine("Greating: " + reply.Message);
+			Console.ReadKey();
+
+			var client2 = new Calculator.CalculatorClient(channel);
+
+			var addResp = await client2.AddAsync(new ArithmeticRequest
+			{
+				FOper = 23.45,
+				SOper = 134.12,
+			});
+
+			Console.WriteLine("Calculator: " + addResp.Result);
+			Console.ReadKey();
+
+		}
+	}
+}
---
+++ GrpcServer/Services/CalculatorService.cs
@@ -0,0 +1,22 @@
+using Grpc.Core;
+using GrpcServer;
+
+namespace GrpcServer.Services
+{
+	public class CalculatorService : Calculator.CalculatorBase
+	{
+		private readonly ILogger<CalculatorService> _logger;
+		public CalculatorService(ILogger<CalculatorService> logger)
+		{
+			_logger = logger;
+		}
+
+		public override Task<ArithmeticResponse> Add(ArithmeticRequest request, ServerCallContext context)
+		{
+			var result = request.FOper + request.SOper;
+			_logger.LogInformation("ќпераци€ сложени€ на сервере выполнилась блест€ще!");
+			return Task.FromResult(new ArithmeticResponse { Result = result });
+
+		}
+	}
+}

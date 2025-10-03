bc13e9b29f3ecce19556ffd69268e42b8f6572ce
Vladislav East <vladislav.sage@yandex.ru>
2025-10-03 20:29:40 +0300

    Добавлены остальные операции

--- GrpcServer/Services/CalculatorService.cs
+++ GrpcServer/Services/CalculatorService.cs
@@ -18,5 +18,28 @@ namespace GrpcServer.Services
 			return Task.FromResult(new ArithmeticResponse { Result = result });
 
 		}
+
+		public override Task<ArithmeticResponse> Substract(ArithmeticRequest request, ServerCallContext context)
+		{
+			var result = request.FOper + request.SOper;
+			_logger.LogInformation("ќпераци€ сложени€ на сервере выполнилась блест€ще!");
+			return Task.FromResult(new ArithmeticResponse { Result = result });
+
+		}
+
+		public override Task<ArithmeticResponse> Multiply(ArithmeticRequest request, ServerCallContext context)
+		{
+			var result = request.FOper + request.SOper;
+			_logger.LogInformation("ќпераци€ сложени€ на сервере выполнилась блест€ще!");
+			return Task.FromResult(new ArithmeticResponse { Result = result });
+
+		}
+		public override Task<ArithmeticResponse> Devided(ArithmeticRequest request, ServerCallContext context)
+		{
+			var result = request.FOper + request.SOper;
+			_logger.LogInformation("ќпераци€ сложени€ на сервере выполнилась блест€ще!");
+			return Task.FromResult(new ArithmeticResponse { Result = result });
+
+		}
 	}
 }
